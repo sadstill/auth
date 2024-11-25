3658db52f7b1cf99c606e4b2f5a02f773390f8a0
Ярослав Андриенко <yandrienko@inno.tech>
2024-11-26 01:34:54 +0300

    реализовал grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	desc "github.com/sadstill/auth/pkg/user/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	fmt.Println(req.GetId())
+	return &desc.GetResponse{}, nil
+}
 
 func main() {
-	fmt.Println("init")
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %d", grpcPort)
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sadstill/auth
 
 go 1.23
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+)
